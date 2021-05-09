50c67c80ae371c7f09f4956f516203b047aedec5
fatimaramos837 <69734132+fatimaramos837@users.noreply.github.com>
2021-05-09 17:22:10 -0600

    Ejercicio2

---
+++ ProgramaOperadores/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaOperadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, genero;
+            int edad;
+            Console.WriteLine("dijite su nombre:");
+            name = Console.ReadLine();
+            Console.WriteLine("dijite su edad:");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("dijite si es hombre(h) o mujer(m):");
+
+        }
+    }
+}
